1b813e5a1248422fb1f167c7e7bddbfed6981b82
ASAD <250046@newuu.uz>
2025-10-23 15:53:51 +0500

    Implement NumPrinter function in problem1.cpp
    Add recursive function to print numbers from 10 to 0.

--- problem1.cpp
+++ problem1.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int NumPrinter(int num=10){
+    if (num==-1)
+    {
+        return 0;
+    }
+    cout << num << endl;
+    NumPrinter(num - 1);
+}
+
+int main() {
+    NumPrinter();
+    return 0;
+}
