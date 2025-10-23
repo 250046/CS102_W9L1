787a5d035ca38ee4346b9f5592716f305eda098c
ASAD <250046@newuu.uz>
2025-10-23 16:58:30 +0500

    Create problem8.cpp

---
+++ problem8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+double SumDigits(int n){
+    if (n==0) {
+        return 0;
+    }
+    return n%10 + SumDigits(n/10);
+}
+
+int main() {
+    int userInt;
+    cin >> userInt;
+    cout << SumDigits(userInt) << endl;
+    return 0;
+}
